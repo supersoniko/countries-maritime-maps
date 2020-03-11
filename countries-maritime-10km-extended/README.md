f651b85ccffb806a27dfd73935aff047670b1449
Sacha Reinert <sacha.reinert@outlook.com>
2020-03-11 10:33:32 +0100

    Added new map countries-maritime-10km-extended

---
+++ countries-maritime-10km-extended/README.md
@@ -0,0 +1,4 @@
+# countries-maritime-10m-extended
+
+Based on [simonepri/geo-maps/countries-maritime-10m](https://github.com/simonepri/geo-maps/tree/master/tpls/npm/countries-maritime) 
+but extended with ISO 3166-1 Alpha-2 country codes.
---
+++ countries-maritime-10km-extended/index.d.ts
@@ -0,0 +1,24 @@
+interface FeatureProperties {
+		A2: string;
+		A3: string;
+}
+
+interface Geometry {
+		coordinates: [number, number][];
+		type: string;
+}
+
+interface Feature {
+		geometry: Geometry;
+		type: string;
+		properties: FeatureProperties;
+}
+
+export interface GeoMap {
+		type: string;
+		features: Feature[];
+}
+
+declare module 'countries-maritime-10m-extended' {
+		export default function(): GeoMap;
+}
